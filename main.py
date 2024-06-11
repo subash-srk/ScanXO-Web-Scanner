f0fe381ea5da685f8e9909a76a3b7779d0e94915
subash-srk <subashthunder28@gmail.com>
2024-06-11 11:50:03 +0530

    add-driver

--- main.py
+++ main.py
@@ -87,7 +87,8 @@ def details():
         return render_template("details.html", URL=req_url, NO_HEADERS=NO_HEADERS, AVAIL_HEADERS=AVAIL_HEADERS, IP=ip)
 
 
-
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
 
